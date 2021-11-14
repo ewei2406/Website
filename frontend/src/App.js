70981b9e930e37e198cfdd43609e84e059dca001
Edward Wei <ewei2406@gmail.com>
2021-11-14 11:21:05 -0500

    Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,35 @@
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Routes, Route, Link
+} from "react-router-dom"
 
 const App = () => {
-    return(
-        <div>
-            Hello from frontend
-        </div>
+    const padding = {
+        padding: 5
+    }
+
+    return (
+        <Router>
+            <div>
+                <Link style={padding} to="/">Home</Link>
+                <Link style={padding} to="/projects">Projects</Link>
+            </div>
+
+            <Routes>
+                <Route path="/projects" element={<div>Hello</div>}>
+                </Route>
+                <Route path="/" element={<div>Home</div>}>
+                </Route>
+                <Route path='*' element={<div>Not Found</div>}>
+                </Route>
+            </Routes>
+
+            <div>
+                <i>Note app, Department of Computer Science 2021</i>
+            </div>
+            
+        </Router>
     )
 }
 
