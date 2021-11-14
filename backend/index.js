d83d29fb126129a466b85fe3e4667467703b554d
Edward Wei <ewei2406@gmail.com>
2021-11-14 00:14:20 -0500

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const App = express()
+
+App.use(express.json())
+App.use(express.static('build'))
+
+const PORT = process.env.PORT || 3001
+
+App.get('/test', (req, res) => {
+    res.send('Hello from backend')
+})
+
+App.listen(PORT, () => {
+    console.log(`App running on Port ${PORT}`)
+})
---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+const App = () => {
+    return(
+        <div>
+            Hello from frontend
+        </div>
+    )
+}
+
+export default App;
